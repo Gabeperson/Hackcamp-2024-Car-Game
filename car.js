c63e534faa6d8af501a13a68818b0dc7a18b8027
Parth Chheda <parth.h.chheda@gmail.com>
2024-11-10 10:38:59 -0800

    added car class, made update and constructor and eventlisteners

---
+++ car.js
@@ -0,0 +1,56 @@
+// width & height
+let CANVAS_WIDTH = 200;
+let CANVAS_HEIGHT = 400;
+
+class Car {
+  constructor() {
+    let direction = Math.PI / 2; // start direction
+    let v = 10; // start velocity
+    let coord = { x: CANVAS_WIDTH / 2, y: CANVAS_HEIGHT / 4 }; // coordunates
+    height = 20; // height
+    width = 20; // width
+    rotationSpeed = 0.15;
+  }
+
+  update() {
+    // update direction on key press
+    if (keys.ArrowLeft) {
+      this.direction += this.rotationSpeed;
+    }
+    if (keys.ArrowRight) {
+      this.direction -= this.rotationSpeed;
+    }
+
+    // update position based on velocity and direction
+    this.x += Math.cos(this.direction) * this.velocity;
+    this.y -= Math.sin(this.direction) * this.velocity;
+
+    // keep car in canvas bounds
+    this.x = Math.max(
+      this.width / 2,
+      Math.min(this.x, CANVAS_WIDTH - this.width / 2)
+    );
+    this.y = Math.max(
+      this.height / 2,
+      Math.min(this.y, CANVAS_HEIGHT - this.height / 2)
+    );
+  }
+}
+
+// canvas
+const canvas = document.getElementById("canvas");
+
+// key events
+let keys = { ArrowRight: false, ArrowLeft: false };
+
+document.addEventListener("keydown", (e) => {
+  if (keys.hasOwnProperty(e.key)) {
+    keys[e.key] = true;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (keys.hasOwnProperty(e.key)) {
+    keys[e.key] = false;
+  }
+});
