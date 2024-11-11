39885bda4de1b3d6891284fe281feeac367d2658
Parth Chheda <parth.h.chheda@gmail.com>
2024-11-10 16:54:05 -0800

    get image for tile done

--- tile.ts
+++ tile.ts
@@ -2,13 +2,43 @@ const SIDE_LENGTH = 50; // change later if needed
 ctx.strokeStyle = "red";
 ctx.lineWidth = 2;
 
-const IMAGE_UP_DOWN = "imgUpDown";
+// // TESING
+// let image = function() {
+//     let elem = new Image();
+//     elem.src = "road.webp";
+//     return elem;
+// }();
 
-const IMAGE_UP_TURN_RIGHT = "imgUpTurnRight";
-const IMAGE_DOWN_TURN_LEFT = "imgDownTurnLeft";
-const IMAGE_DOWN_TURN_RIGHT = "imgDownTurnRight";
-const IMAGE_UP_TURN_LEFT = "imgUpTurnLeft";
-const IMAGE_SIDE = "imgSide";
+const IMAGE_UP_DOWN = function () {
+  let elem = new Image();
+  elem.src = "./road_images/image_top_down.webp";
+  return elem;
+};
+const IMAGE_UP_TURN_RIGHT = function () {
+  let elem = new Image();
+  elem.src = "./road_images/image_left_up.webp";
+  return elem;
+};
+const IMAGE_DOWN_TURN_LEFT = function () {
+  let elem = new Image();
+  elem.src = "./road_images/image_up_right.webp";
+  return elem;
+};
+const IMAGE_DOWN_TURN_RIGHT = function () {
+  let elem = new Image();
+  elem.src = "./road_images/image-down-right.webp";
+  return elem;
+};
+const IMAGE_UP_TURN_LEFT = function () {
+  let elem = new Image();
+  elem.src = "./road_images/image-left-down.webp";
+  return elem;
+};
+const IMAGE_SIDE = function () {
+  let elem = new Image();
+  elem.src = "./road_images/image-side.webp";
+  return elem;
+};
 
 class Tile {
   edge: { l: boolean; r: boolean; t: boolean; b: boolean };
