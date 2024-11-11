f509bf5999d63e7823301ae3732dc37fc6ab7f85
Parth Chheda <parth.h.chheda@gmail.com>
2024-11-10 16:21:50 -0800

    refactored procgen and added get image

--- procgen.ts
+++ procgen.ts
@@ -1,205 +1,183 @@
 const TILE_SIZE = 10;
 
-// Procedural generation
 enum Direction {
-    Left,
-    Right,
-    Top,
-    Bottom,
+  Left,
+  Right,
+  Top,
+  Bottom,
 }
 
-// Randomize array in-place using Durstenfeld shuffle algorithm
 function shuffleArray(array) {
-    for (var i = array.length - 1; i >= 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+  for (var i = array.length - 1; i >= 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
 
 class CustomMap {
-    map: Map<number, Map<number, Tile>>;
-    constructor() {
-        this.map = new Map();
-    }
-    size() {
-        let s = 0;
-        for (let [_unused, innermap] of this.map) {
-            s += innermap.size;
-        }
-        return s;
-    }
-    get(key: [number, number]) {
-        let [x, y] = key;
-        let col = this.map.get(x);
-        if (col == undefined) {
-            return undefined;
-        }
-        let tile = col.get(y);
-        return tile;
-    }
-    set(key: [number ,number], tile: Tile) {
-        let [x, y] = key;
-        let col = this.map.get(x);
-        if (col == undefined) {
-            let map = new Map();
-            this.map.set(x, map);
-            col = map;
-        }
-        col.set(y, tile);
-    }
-    delete(key: [number, number]) {
-        let [x, y] = key;
-        let col = this.map.get(x);
-        if (col == undefined) {
-            console.error("Deleted coord that doesn't exist!! (bug in map impl)");
-            return;
-        }
-        col.delete(y);
-        if (col.size == 0) {
-            this.map.delete(x);
-            console.log("removed size 0");
-        }
-    }
-    *entries() {
-        for (let [x, col] of this.map) {
-            for (let [y, tile] of col) {
-                yield [[x, y], tile] as [[number, number], Tile];
-            }
-        }
-    }
+  map: Map<number, Map<number, Tile>>;
+  constructor() {
+    this.map = new Map();
+  }
 
-    [Symbol.iterator]() {
-        return this.entries();
+  size() {
+    let s = 0;
+    for (let [_unused, innermap] of this.map) {
+      s += innermap.size;
     }
+    return s;
+  }
+
+  get(vec: Vec2) {
+    let col = this.map.get(vec.x);
+    if (col == undefined) {
+      return undefined;
+    }
+    return col.get(vec.y);
+  }
+
+  set(vec: Vec2, tile: Tile) {
+    let col = this.map.get(vec.x);
+    if (col == undefined) {
+      let map = new Map();
+      this.map.set(vec.x, map);
+      col = map;
+    }
+    col.set(vec.y, tile);
+  }
+
+  delete(vec: Vec2) {
+    let col = this.map.get(vec.x);
+    if (col == undefined) {
+      console.error("Deleted coord that doesn't exist!! (bug in map impl)");
+      return;
+    }
+    col.delete(vec.y);
+    if (col.size == 0) {
+      this.map.delete(vec.x);
+      console.log("removed size 0");
+    }
+  }
+
+  *entries() {
+    for (let [x, col] of this.map) {
+      for (let [y, tile] of col) {
+        yield [new Vec2(x, y), tile] as [Vec2, Tile];
+      }
+    }
+  }
+
+  [Symbol.iterator]() {
+    return this.entries();
+  }
 }
 
 type TileMap = CustomMap;
-// let weights = [1, 1, 1];
+
 let dir_map = new Map<Direction, Direction[]>([
-    [Direction.Top, [Direction.Left, Direction.Top, Direction.Right]],
-    [Direction.Left, [Direction.Top, Direction.Left, Direction.Bottom]],
-    [Direction.Right, [Direction.Top, Direction.Right, Direction.Bottom]],
-    [Direction.Bottom, [Direction.Left, Direction.Bottom, Direction.Right]],
+  [Direction.Top, [Direction.Left, Direction.Top, Direction.Right]],
+  [Direction.Left, [Direction.Top, Direction.Left, Direction.Bottom]],
+  [Direction.Right, [Direction.Top, Direction.Right, Direction.Bottom]],
+  [Direction.Bottom, [Direction.Left, Direction.Bottom, Direction.Right]],
 ]);
 
-function dir_to_offset(dir: Direction): [number, number] {
-    switch (dir) {
-        case Direction.Bottom:
-            return [0, -TILE_SIZE]
-        case Direction.Top:
-            return [0, TILE_SIZE]
-        case Direction.Left:
-            return [-TILE_SIZE, 0]
-        case Direction.Right:
-            return [TILE_SIZE, 0]
-    }
+function dir_to_offset(dir: Direction): Vec2 {
+  switch (dir) {
+    case Direction.Bottom:
+      return new Vec2(0, -TILE_SIZE);
+    case Direction.Top:
+      return new Vec2(0, TILE_SIZE);
+    case Direction.Left:
+      return new Vec2(-TILE_SIZE, 0);
+    case Direction.Right:
+      return new Vec2(TILE_SIZE, 0);
+  }
 }
+
 function dir_opposite(dir: Direction): Direction {
-    switch (dir) {
-        case Direction.Bottom:
-            return Direction.Top
-        case Direction.Top:
-            return Direction.Bottom
-        case Direction.Left:
-            return Direction.Right
-        case Direction.Right:
-            return Direction.Left
-    }
+  switch (dir) {
+    case Direction.Bottom:
+      return Direction.Top;
+    case Direction.Top:
+      return Direction.Bottom;
+    case Direction.Left:
+      return Direction.Right;
+    case Direction.Right:
+      return Direction.Left;
+  }
 }
-
-function addCoord(coord: [number, number], offset: [number, number]):
-    [number, number] {
-        return [coord[0]+offset[0], coord[1]+offset[1]]
-}
-
-// function weighted_random(items, weights) {
-//     for (let i = 1; i < weights.length; i++)
-//         weights[i] += weights[i - 1];
-    
-//     var random = Math.random() * weights[weights.length - 1];
-//     var i: number;
-//     for (i = 0; i < weights.length; i++)
-//         if (weights[i] > random)
-//             break;
-    
-//     return items[i];
-// }
 
 function updateTileBoundary(tile: Tile, dir: Direction) {
-    switch (dir) {
-        case Direction.Bottom:
-            tile.edge.b = false;
-        case Direction.Top:
-            tile.edge.t = false;
-        case Direction.Left:
-            tile.edge.l = false;
-        case Direction.Right:
-            tile.edge.r = false;
-    }
+  switch (dir) {
+    case Direction.Bottom:
+      tile.edge.b = false;
+    case Direction.Top:
+      tile.edge.t = false;
+    case Direction.Left:
+      tile.edge.l = false;
+    case Direction.Right:
+      tile.edge.r = false;
+  }
 }
 
 let __generate_id: number = 1;
 
-// Dir is "open direction" of the last tile.
-// returns true if success. false if backtrack.
-function generate(map: TileMap, from: [number, number],
-    dir: Direction, car: [number, number], view_dist: number, gen_dist: number): boolean {
-    console.log("Ran");
-    let distX = Math.abs(car[0] - from[0]);
-    let distY = Math.abs(car[1] - from[1])
-    let sqDist = distX*distX+distY*distY;
-    let offset = dir_to_offset(dir);
-    let newLoc: [number, number] = addCoord(from, offset);
-    // console.log(newLoc);
-    // console.log(map);
-    if (map.get(newLoc) != undefined) {
-        console.log("Ran2");
-        return false; // This tile already has a road. We need to backtrack!
-    }
-    if (sqDist > gen_dist) {
-        console.log("Ran3");
-        return true; // we're good. no backtracking needed
-    }
-    if (sqDist < view_dist) {
-        console.log("Ran4");
+function generate(
+  map: TileMap,
+  from: Vec2,
+  dir: Direction,
+  car: Vec2,
+  view_dist: number,
+  gen_dist: number
+): boolean {
+  console.log("Ran");
+  let diff = car.subtract(from);
+  let sqDist = diff.dot(diff);
+  let offset = dir_to_offset(dir);
+  let newLoc = from.add(offset);
 
-        return false; // We're too close, user might see the generation.
+  if (map.get(newLoc) != undefined) {
+    console.log("Ran2");
+    return false; // This tile already has a road. We need to backtrack!
+  }
+  if (sqDist > gen_dist) {
+    console.log("Ran3");
+    return true; // we're good. no backtracking needed
+  }
+  if (sqDist < view_dist) {
+    console.log("Ran4");
+    return false; // We're too close, user might see the generation.
+  }
+
+  // Generation
+  let items = dir_map.get(dir)!.slice(0);
+  shuffleArray(items);
+  for (let direction of items) {
+    let offsetNew = dir_to_offset(direction);
+    let newTile = new Tile(newLoc.x, newLoc.y);
+    // Update direction we came from
+    updateTileBoundary(newTile, dir_opposite(dir));
+    // Update direction we are going to
+    updateTileBoundary(newTile, direction);
+    newTile.id = __generate_id;
+    map.set(newLoc, newTile);
+    __generate_id++;
+    let nextLoc = newLoc.add(offsetNew);
+    let res = generate(map, nextLoc, direction, car, view_dist, gen_dist);
+    console.log(res);
+    if (res) {
+      // Succeeded, we also return true.
+      return true;
+    } else {
+      // Failure bubbled up, we test next option.
+      map.delete(newLoc);
+      console.log("ran!!!!!");
+      __generate_id--;
+      continue;
     }
-    // Generation
-    let items = dir_map.get(dir)!.slice(0);
-    shuffleArray(items);
-    for (let direction of items) {
-        let offsetNew = dir_to_offset(direction);
-        let newTile = new Tile(newLoc[0], newLoc[1]);
-        // Update direction we came from
-        updateTileBoundary(newTile, dir_opposite(dir));
-        // Update direction we are going to
-        updateTileBoundary(newTile, direction);
-        newTile.id = __generate_id;
-        map.set(newLoc, newTile);
-        __generate_id++;
-        let res = generate(
-            map, 
-            addCoord(newLoc, offsetNew),
-            direction,
-            car,
-            view_dist,
-            gen_dist,
-        );
-        console.log(res);
-        if (res) {
-            // Succeeded, we also return true.
-            return true;
-        } else {
-            // Failure bubbled up, we test next option.
-            map.delete(newLoc);
-            console.log("ran!!!!!")
-            __generate_id--;
-            continue;
-        }
-    }
-    // Failed to find route. We're surrounded. Backtrack.
-    return false;
+  }
+  // Failed to find route. We're surrounded. Backtrack.
+  return false;
 }
--- tile.ts
+++ tile.ts
@@ -2,6 +2,14 @@ const SIDE_LENGTH = 50; // change later if needed
 ctx.strokeStyle = "red";
 ctx.lineWidth = 2;
 
+const IMAGE_UP_DOWN = "imgUpDown";
+
+const IMAGE_UP_TURN_RIGHT = "imgUpTurnRight";
+const IMAGE_DOWN_TURN_LEFT = "imgDownTurnLeft";
+const IMAGE_DOWN_TURN_RIGHT = "imgDownTurnRight";
+const IMAGE_UP_TURN_LEFT = "imgUpTurnLeft";
+const IMAGE_SIDE = "imgSide";
+
 class Tile {
   edge: { l: boolean; r: boolean; t: boolean; b: boolean };
   x: number;
@@ -25,5 +33,31 @@ class Tile {
       top: this.y,
       bottom: this.y + this.sideLength,
     };
+  }
+
+  getImage() {
+    if (!this.edge.t && !this.edge.b) {
+      return IMAGE_UP_DOWN;
+    }
+
+    if (!this.edge.r && !this.edge.l) {
+      return IMAGE_SIDE;
+    }
+
+    if (!this.edge.t && !this.edge.r) {
+      return IMAGE_DOWN_TURN_RIGHT;
+    }
+
+    if (!this.edge.t && !this.edge.l) {
+      return IMAGE_DOWN_TURN_LEFT;
+    }
+
+    if (!this.edge.r && !this.edge.b) {
+      return IMAGE_UP_TURN_RIGHT;
+    }
+
+    if (!this.edge.l && !this.edge.b) {
+      return IMAGE_UP_TURN_LEFT;
+    }
   }
 }
